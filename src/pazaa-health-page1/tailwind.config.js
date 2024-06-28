9d131ba3b4eba401daf0d925027ff7e47006e2c4
Barklegne Eshetu <barklegne.eshetu@gmail.com>
2024-06-28 15:34:20 +0300

    pazza-health hero section added

--- src/pazaa-health-page1/tailwind.config.js
+++ src/pazaa-health-page1/tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		screens: {
+			xs: '375px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
+		extend: {
+			fontFamily: {
+				epilouge: ['Epilogue', 'serif'],
+				lato: ['Lato', 'sans-serif'],
+			},
+			colors: {
+				primary: '#4A56A7',
+				primaryLight: '#959DD2',
+				secondary: '#F18990',
+				bgGreyMedium: '#F5F5F5',
+				// secondaryLight: '#F18990',
+				// golden: '#EEC151',
+				grey: '#333333',
+			},
+			backgroundImage: {
+				'hero-background': "url('/src/assets/hero-img.svg)",
+			},
+		},
 	},
 	plugins: [],
 }
