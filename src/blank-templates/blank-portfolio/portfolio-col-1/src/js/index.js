882e16f708b339d509d74cc209f0e0ad93b91526
Barklegne Eshetu <barklegne.eshetu@gmail.com>
2024-07-09 12:48:15 +0300

    5 portfolio blank templates completed

---
+++ src/blank-templates/blank-portfolio/portfolio-col-1/src/js/index.js
@@ -0,0 +1,32 @@
+// Section loader
+function loadHTML(sectionId, filePath) {
+	fetch(filePath)
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById(sectionId).innerHTML = data
+		})
+		.catch((error) => console.error('Error loading section:', error))
+}
+
+loadHTML('footer', 'sections/footer.html')
+
+// hamburger menu toggler
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+	btn.classList.toggle('open')
+	nav.classList.toggle('flex')
+	nav.classList.toggle('hidden')
+})
+
+// scroll animation
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth',
+		})
+	})
+})
