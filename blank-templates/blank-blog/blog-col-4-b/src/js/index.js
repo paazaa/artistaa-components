184ed46d65a217d34657272e73828e0bccd7993f
Barklegne Eshetu <barklegne.eshetu@gmail.com>
2024-07-22 20:18:41 +0300

    blog-col-4-b completed

---
+++ blank-templates/blank-blog/blog-col-4-b/src/js/index.js
@@ -0,0 +1,33 @@
+// Section loader
+function loadHTML(sectionId, filePath) {
+	fetch(filePath)
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById(sectionId).innerHTML = data
+		})
+		.catch((error) => console.error('Error loading section:', error))
+}
+
+loadHTML('blog', 'sections/blog.html')
+loadHTML('footer', 'sections/footer.html')
+
+// hamburger menu toggler
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+	btn.classList.toggle('open')
+	nav.classList.toggle('flex')
+	nav.classList.toggle('hidden')
+})
+
+// scroll animation
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth',
+		})
+	})
+})
